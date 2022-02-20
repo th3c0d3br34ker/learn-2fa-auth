4e40ac8895b6b99ef2bf7a838e44ffe43eb81cbb
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2022-02-20 00:48:45 +0000

    updated: favicon

--- container/layout.js
+++ container/layout.js
@@ -8,9 +8,6 @@ const Layout = (props) => {
   return (
     <>
       <Head>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <title>{title}</title>
       </Head>
 
